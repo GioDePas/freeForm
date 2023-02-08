08d4c61bf4d05a5e37839c962892ec841b5f9eb8
giorgio.depascale <giorgio.depascale@outlook.com>
2023-02-08 14:22:50 +0100

    first

---
+++ src/main/java/com/freeForm/FreeFormApplication.java
@@ -0,0 +1,13 @@
+package com.freeForm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreeFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreeFormApplication.class, args);
+	}
+
+}
