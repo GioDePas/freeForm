7a3f833161b45f3cf2793a3de0fb49e5e885455a
giorgio.depascale <giorgio.depascale@outlook.com>
2023-02-09 10:54:45 +0100

    security filter chain

--- src/main/java/com/freeForm/controller/UserController.java
+++ src/main/java/com/freeForm/controller/UserController.java
@@ -1,4 +1,40 @@
 package com.freeForm.controller;
 
+import com.freeForm.entity.User;
+import com.freeForm.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable Long id, @RequestBody User user) {
+        return userService.updateUser(id, user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+    }
 }
--- src/main/java/com/freeForm/service/UserService.java
+++ src/main/java/com/freeForm/service/UserService.java
@@ -1,4 +1,41 @@
 package com.freeForm.service;
 
+import com.freeForm.entity.User;
+import com.freeForm.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User user) {
+        User currentUser = userRepository.findById(id).orElse(null);
+        if (currentUser != null) {
+            currentUser.setUsername(user.getUsername());
+            currentUser.setPassword(user.getPassword());
+        }
+        return userRepository.save(currentUser);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
