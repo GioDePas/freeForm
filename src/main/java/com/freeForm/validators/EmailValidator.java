29a95a9d24a9a65d4f9fca08cc44bf2657c242b9
giorgio.depascale <giorgio.depascale@outlook.com>
2023-03-01 18:00:56 +0100

    stuff

--- src/main/java/com/freeForm/validators/EmailValidator.java
+++ src/main/java/com/freeForm/validators/EmailValidator.java
@@ -1,26 +1,21 @@
 package com.freeForm.validators;
 
 import com.freeForm.annotations.IE002;
+import com.freeForm.constants.Patterns;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.util.regex.Pattern;
 
 public class EmailValidator implements ConstraintValidator<IE002, String> {
-    public static final String EMAIL_PATTERN =
-            "^[a-zA-Z0-9_+&*-]+(?:\\." +
-            "[a-zA-Z0-9_+&*-]+)*@" +
-            "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
-            "A-Z]{2,7}$";
 
     @Override
     public boolean isValid(String  email, ConstraintValidatorContext context) {
         return email != null && isValidEmail(email);
     }
 
-    public static boolean isValidEmail(String email) {
-        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+    public boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile(Patterns.EMAIL_PATTERN);
         return pattern.matcher(email).matches();
     }
-
 }
--- src/main/java/com/freeForm/validators/PasswordValidator.java
+++ src/main/java/com/freeForm/validators/PasswordValidator.java
@@ -1,13 +1,13 @@
 package com.freeForm.validators;
 
 import com.freeForm.annotations.IP008;
+import com.freeForm.constants.Patterns;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.util.regex.Pattern;
 
 public class PasswordValidator implements ConstraintValidator<IP008, String> {
-    public static final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[_@#$%^&+=-])(?=.*[0-9])(?=\\S+$).{8,}$";
 
     @Override
     public boolean isValid(String password, ConstraintValidatorContext context) {
@@ -15,7 +15,7 @@ public class PasswordValidator implements ConstraintValidator<IP008, String> {
     }
 
     private boolean isValidPassword(String password) {
-        Pattern pattern = Pattern.compile(PASSWORD_PATTERN);
+        Pattern pattern = Pattern.compile(Patterns.PASSWORD_PATTERN);
         return pattern.matcher(password).matches();
     }
 }
